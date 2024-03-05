5f1f35ac26b517e9436ee380b6135d84fc61e0bc
Valery Shcherbinin <fundroin@mail.ru>
2024-03-05 09:03:53 +0500

    Добавялет настройки для проекта и конфигурацию для БД Sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from "@nestjs/sequelize";
+
+import mainConfig from './core/config/main.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+      ConfigModule.forRoot({
+          load: [mainConfig],
+          envFilePath: ['.env'],
+          isGlobal: true
+      }),
+      SequelizeModule.forRoot({
+          username: process.env.DATABASE_USERNAME,
+          password: process.env.DATABASE_PASSWORD,
+          database: process.env.DATABASE_NAME,
+          host: process.env.DATABASE_HOST,
+          port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+          dialect: 'postgres',
+          autoLoadModels: true,
+          synchronize: true
+      })
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from "@nestjs/config";
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    credentials: true,
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    origin: `${configService.get('CLIENT_URL')}:${configService.get('CLIENT_PORT')}`
+  })
+
+  const port = configService.get<number>('API_PORT');
+
+  await app.listen(port, () => {
+    console.log(`Server started on port: ${port}`)
+  });
 }
+
 bootstrap();
