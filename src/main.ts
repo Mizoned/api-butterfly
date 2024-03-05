1da3994de1f544c2e5ab5c4b0b2f2c242544ce2d
Valery Shcherbinin <fundroin@mail.ru>
2024-03-05 10:34:54 +0500

    Добавляет Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from "@nestjs/config";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
@@ -14,7 +15,16 @@ async function bootstrap() {
     credentials: true,
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
     origin: `${configService.get('CLIENT_URL')}:${configService.get('CLIENT_PORT')}`
-  })
+  });
+
+  const config = new DocumentBuilder()
+      .setTitle('REST API Butterfly')
+      .setDescription('Документация по API Butterfly')
+      .setVersion('1.0.0')
+      .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, document);
 
   const port = configService.get<number>('API_PORT');
 
