485179fec6f0c5adff330886bea0799e2dcded1f
Valery Shcherbinin <fundroin@mail.ru>
2024-11-12 19:14:06 +0500

    Добавляет вспомогательные методы в utils

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -10,3 +10,30 @@ export const comparePassword = async (
 ): Promise<boolean> => {
 	return await bcrypt.compare(password, hashedPassword);
 };
+
+
+export const isCurrentMonth = (date: Date) => {
+	const currentDate = new Date();
+
+	return currentDate.getFullYear() === date.getFullYear() && currentDate.getMonth() === date.getMonth();
+}
+
+export const getWeekDays = (date: Date): Date[] => {
+	const current = new Date(date);
+
+	// Определяем текущий день недели (0 - воскресенье)
+	const dayOfWeek = current.getDay();
+
+	// Смещаем текущий день на понедельник, если он не является первым днём недели
+	const diffToMonday = dayOfWeek === 0 ? -6 : 1 - dayOfWeek;
+	current.setDate(current.getDate() + diffToMonday);
+
+	const weekDays: Date[] = [];
+
+	for (let i = 0; i < 7; i++) {
+		weekDays.push(new Date(current));
+		current.setDate(current.getDate() + 1);
+	}
+
+	return weekDays;
+}
