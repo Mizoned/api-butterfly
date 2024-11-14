6f158b8cf1bdf6873bfc2a0a79f008d88135a7c9
Valery Shcherbinin <fundroin@mail.ru>
2024-11-14 10:19:51 +0500

    Удаляет лишние импорты в модуль статистики

--- src/modules/statistics/statistics.module.ts
+++ src/modules/statistics/statistics.module.ts
@@ -4,13 +4,9 @@ import { StatisticsService } from './statistics.service';
 import { ProductsModule } from '@modules/products/products.module';
 import { CustomersModule } from '@modules/customers/customers.module';
 import { SchedulesModule } from '@modules/schedules/schedules.module';
-import {SequelizeModule} from '@nestjs/sequelize';
-import { ScheduleModel } from '@modules/schedules/models/schedule.model';
-import { ScheduleProductsModel } from '@modules/schedules/models/schedule-products.model';
 
 @Module({
     imports: [
-        SequelizeModule.forFeature([ScheduleModel, ScheduleProductsModel]),
         ProductsModule,
         CustomersModule,
         SchedulesModule
